0cd6c66c240abf2fd20017bea6e36991950d4695
Jason McHuff <github@jasonmchuff.net>
2023-04-11 01:45:14 -0700

    log decoding rates to decode_rates.csv
    does it once/minute to file in capture dir in config.json order
    
    also turning off printing status messages

--- trunk-recorder/main.cc
+++ trunk-recorder/main.cc
@@ -1379,9 +1379,21 @@ void retune_system(System *sys) {
   }
 }
 
-void check_message_count(float timeDiff) {
+void check_message_count(float timeDiff, bool logit) {
   plugman_setup_config(sources, systems);
   plugman_system_rates(systems, timeDiff);
+  char decode_rates_file[255]; 
+  std::ofstream myfile;
+  if (logit) {
+      time_t now = time(NULL);
+      int nchars;
+      nchars = snprintf(decode_rates_file, 255, "%s/decode_rates.csv", config.capture_dir.c_str());
+      myfile.open(decode_rates_file, std::ofstream::app);
+      if (myfile.is_open()) {
+        myfile.fill('0');
+        myfile << now;
+      }
+  }
 
   for (std::vector<System *>::iterator it = systems.begin(); it != systems.end(); ++it) {
     System_impl *sys = (System_impl *)*it;
@@ -1389,6 +1401,10 @@ void check_message_count(float timeDiff) {
     if ((sys->get_system_type() != "conventional") && (sys->get_system_type() != "conventionalP25") && (sys->get_system_type() != "conventionalDMR")) {
       int msgs_decoded_per_second = std::floor(sys->message_count / timeDiff);
       sys->set_decode_rate(msgs_decoded_per_second);
+
+      if (logit && myfile.is_open()) {
+        myfile << "," << msgs_decoded_per_second;
+      }
 
       if (msgs_decoded_per_second < 2) {
 
@@ -1419,6 +1435,9 @@ void check_message_count(float timeDiff) {
     }
     sys->message_count = 0;
   }
+  if (logit && myfile.is_open())
+    myfile << "\n";
+    myfile.close();
 }
 
 void monitor_messages() {
@@ -1427,6 +1446,7 @@ void monitor_messages() {
   System *sys;
 
   time_t last_status_time = time(NULL);
+  bool logit = false;
   time_t last_decode_rate_check = time(NULL);
   time_t management_timestamp = time(NULL);
   time_t current_time = time(NULL);
@@ -1500,9 +1520,12 @@ void monitor_messages() {
     current_time = time(NULL);
 
     float decode_rate_check_time_diff = current_time - last_decode_rate_check;
+    float print_status_time_diff = current_time - last_status_time;
 
     if (decode_rate_check_time_diff >= 3.0) {
-      check_message_count(decode_rate_check_time_diff);
+      if (print_status_time_diff >= 60.0) { logit = true; last_status_time = current_time; }
+      check_message_count(decode_rate_check_time_diff, logit);
+      logit = false;
       last_decode_rate_check = current_time;
       for (vector<System *>::iterator sys_it = systems.begin(); sys_it != systems.end(); sys_it++) {
         System *system = *sys_it;
@@ -1510,13 +1533,6 @@ void monitor_messages() {
           system->clear_stale_talkgroup_patches();
         }
       }
-    }
-
-    float print_status_time_diff = current_time - last_status_time;
-
-    if (print_status_time_diff > 200) {
-      last_status_time = current_time;
-      print_status();
     }
   }
 }
